feb5a321e10324f08fcdefa4b60a7faccc4caca0
udaya276 <udaya276@gmail.com>
2023-01-04 21:43:26 +0530

    Revert "class 10 training pipeline complete"
    This reverts commit 9419ef45a72493a29518ec4942d3df2a6f3743de.

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -7,12 +7,6 @@ from sensor.components.data_ingestion import DataIngestionConfig
 from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.components.data_transformation import DataTransformation
-from sensor.components.model_trainer import ModelTrainer
-from sensor.components.model_evaluation import ModelEvaluation
-from sensor.components.model_pusher import ModelPusher
-#from sensor.cloud_storage.s3_syncer import S3Sync
-from sensor.constant.s3_bucket import TRAINING_BUCKET_NAME
-from sensor.constant.training_pipeline import SAVED_MODEL_DIR
 
 class TrainPipeline:
     is_pipeline_running = False
@@ -73,33 +67,12 @@ class TrainPipeline:
         except Exception as e:
             raise SensorException(e,sys)
 
-    def sync_artifact_dir_to_s3(self):
-        try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
-            self.s3_sync.sync_folder_to_s3(folder = self.training_pipeline_config.artifact_dir,aws_buket_url=aws_buket_url)
-        except Exception as e:
-            raise SensorException(e,sys)
-            
-    def sync_saved_model_dir_to_s3(self):
-        try:
-            aws_buket_url = f"s3://{TRAINING_BUCKET_NAME}/{SAVED_MODEL_DIR}"
-            self.s3_sync.sync_folder_to_s3(folder = SAVED_MODEL_DIR,aws_buket_url=aws_buket_url)
-        except Exception as e:
-            raise SensorException(e,sys)
-
     def run_pipeline(self):
         try:
             TrainPipeline.is_pipeline_running = True
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact:DataValidationArtifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
-
-            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
-            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
-            if not model_eval_artifact.is_model_accepted:
-                raise Exception("Training model is not better than the best model")
-            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
-
         except Exception as e:
             self.sync_artifact_dir_to_s3()
             TrainPipeline.is_pipeline_running = False
